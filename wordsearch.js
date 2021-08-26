41d83959bcc733271bae364e823f25d61f72ffcd
Sara Sabet <sara.ilbeigi@gmail.com>
2021-08-26 01:54:36 +0000

    pp

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,57 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    let string = [];
+    
+    for (let i = 0 ; i < letters[i].length; i++)
+    {
+        let temp;
+        for(let j= 0 ; j < letters.length; j++)
+        {
+            temp += letters[j][i];
+        }
+        string.push(temp);
     }
+    
+    for (const each of string) {
+        if(each.includes(word))
+        {
+            return true;
+        }
+        
+    }
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+        return true
+    }
+   
+    }
+    return false;
 }
 
+const word = [
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ]
+
+// const wordSearch= [
+//     ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//     ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//     ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//     ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//     ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+//   ]  
+  console.log(wordSearch(word, 'FRANK') ) 
+
 module.exports = wordSearch
+   
